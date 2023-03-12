6b8f71400ea0ac90d4669dc4e1a13ba1acf169f0
Sasha <alexandra.aslobodian@gmail.com>
2023-03-12 12:36:04 -0400

    created first lab to implement loosely coupling structure

---
+++ lab-00-coupling/src/main/java/com/learn/loosely/BalanceManager.java
@@ -0,0 +1,10 @@
+package com.learn.loosely;
+
+public class BalanceManager {
+    public boolean checkout(
+            // add method parameters
+    ){
+        // implement checkout business
+        return false;
+    }
+}
