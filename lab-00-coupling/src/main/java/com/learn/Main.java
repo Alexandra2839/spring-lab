be7eb5229ec341d06f3442ab88af86dd58b8ec2b
Sasha <alexandra.aslobodian@gmail.com>
2023-03-12 12:44:03 -0400

    updated main runner class to test loosely coupling structure

--- lab-00-coupling/src/main/java/com/learn/Main.java
+++ lab-00-coupling/src/main/java/com/learn/Main.java
@@ -1,5 +1,7 @@
 package com.learn;
-import com.cydeo.tightly.BalanceService;
+import com.learn.loosely.Balance;
+import com.learn.loosely.BalanceManager;
+import com.learn.tightly.BalanceService;
 
 import java.math.BigDecimal;
 import java.util.UUID;
@@ -8,17 +10,24 @@ public class Main {
     public static void main(String[] args) {
         UUID user = UUID.randomUUID();
 
-        CustomerBalance customerBalance = new CustomerBalance(user, BigDecimal.ZERO);
-        GiftCardBalance giftCardBalance = new GiftCardBalance(user, BigDecimal.ZERO);
+        Balance customerBalance = new CustomerBalance(user, BigDecimal.ZERO);
+        Balance giftCardBalance = new GiftCardBalance(user, BigDecimal.ZERO);
 
         customerBalance.addBalance(new BigDecimal(150));
         giftCardBalance.addBalance(new BigDecimal(120));
 
-        BalanceService balanceService =
-                new BalanceService(customerBalance, giftCardBalance);
+//        BalanceService balanceService =
+//                new BalanceService(customerBalance, giftCardBalance);
 
-        System.out.println(balanceService.checkoutFromCustomerBalance(new BigDecimal(80)));
-        System.out.println(balanceService.checkoutFromGiftBalance(new BigDecimal(80)));
+
+        BalanceManager balanceManager = new BalanceManager();
+        System.out.println(balanceManager.checkout(customerBalance, new BigDecimal(80)));
+        System.out.println(balanceManager.checkout(giftCardBalance, new BigDecimal(50)));
+
+
+
+//        System.out.println(balanceService.checkoutFromCustomerBalance(new BigDecimal(80)));
+//        System.out.println(balanceService.checkoutFromGiftBalance(new BigDecimal(80)));
     }
 
 }
