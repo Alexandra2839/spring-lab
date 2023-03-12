422bc9c088f749bda4b2f4bcdda70d4a9ba7b3fb
Sasha <alexandra.aslobodian@gmail.com>
2023-03-12 13:10:10 -0400

    added @component to cart repository

--- lab-02-di/src/main/java/com/learn/repository/CartRepositoryImpl.java
+++ lab-02-di/src/main/java/com/learn/repository/CartRepositoryImpl.java
@@ -6,6 +6,8 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class CartRepositoryImpl implements CartRepository{
+
+
     public boolean addCartDatabase(Product product, int quantity) {
         System.out.println(product.getName() + " is added to database");
         return true;
