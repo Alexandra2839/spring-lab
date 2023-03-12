8d7df3f236a9c04c6f9433156841910cf415d32f
Sasha <alexandra.aslobodian@gmail.com>
2023-03-12 12:43:29 -0400

    updated customer and gift card balance to extend parent balance class

--- lab-00-coupling/src/main/java/com/learn/CustomerBalance.java
+++ lab-00-coupling/src/main/java/com/learn/CustomerBalance.java
@@ -1,25 +1,26 @@
 package com.learn;
 
+import com.learn.loosely.Balance;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.math.BigDecimal;
 import java.util.UUID;
 
+
+
+
+
 @Getter
 @Setter
-public class CustomerBalance{
-
-    private UUID userId;
-    private BigDecimal amount;
+public class CustomerBalance extends Balance {
 
     public CustomerBalance(UUID userId, BigDecimal amount) {
-        this.userId = userId;
-        this.amount = amount;
+        super(userId, amount);
     }
 
     public BigDecimal addBalance(BigDecimal amount) {
-        setAmount(this.amount.add(amount));
-        return this.amount;
+        setAmount(amount.add(amount));
+        return amount;
     }
 }
