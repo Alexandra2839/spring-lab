27c86e46c6a6bf1d5ca60aa2473e6c8ec7cc226e
Sasha <alexandra.aslobodian@gmail.com>
2023-03-12 13:10:47 -0400

    added @component to StockServiceImpl

--- lab-02-di/src/main/java/com/learn/service/StockServiceImpl.java
+++ lab-02-di/src/main/java/com/learn/service/StockServiceImpl.java
@@ -3,6 +3,7 @@ package com.learn.service;
 import com.learn.model.Product;
 import org.springframework.stereotype.Component;
 
+
 @Component
 public class StockServiceImpl implements StockService{
     @Override
