47d2a267f54d8310c1cacac469f446bd477c97ad
Sasha <alexandra.aslobodian@gmail.com>
2023-03-19 10:51:07 -0400

    Product repository added

---
+++ lab-04-thymeleaf/src/main/java/com/learn/spring04thymeleaf/repository/ProductRepository.java
@@ -0,0 +1,15 @@
+package com.learn.spring04thymeleaf.repository;
+
+
+import com.learn.spring04thymeleaf.model.Product;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface ProductRepository {
+    boolean save(Product product);
+
+    Product findProductById(UUID productId);
+
+    List<Product> findAll();
+}
