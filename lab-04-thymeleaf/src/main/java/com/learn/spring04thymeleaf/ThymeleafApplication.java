abd77cd047ec43e7113e3ee719ba0a4298b97a63
Sasha <alexandra.aslobodian@gmail.com>
2023-03-19 10:51:35 -0400

    main runner commit

---
+++ lab-04-thymeleaf/src/main/java/com/learn/spring04thymeleaf/ThymeleafApplication.java
@@ -0,0 +1,13 @@
+package com.learn.spring04thymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeleafApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThymeleafApplication.class, args);
+    }
+
+}
