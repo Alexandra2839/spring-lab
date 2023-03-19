d943d28bbb7b4230c53d2806e28071176c3721e4
Sasha <alexandra.aslobodian@gmail.com>
2023-03-19 15:28:52 -0400

    added /addToCart to controller

--- lab-04-thymeleaf/src/main/java/com/learn/spring04thymeleaf/controller/CartController.java
+++ lab-04-thymeleaf/src/main/java/com/learn/spring04thymeleaf/controller/CartController.java
@@ -3,12 +3,24 @@ package com.learn.spring04thymeleaf.controller;
 
 
 import static com.learn.spring04thymeleaf.service.impl.CartServiceImpl.CART;
+
+import com.learn.spring04thymeleaf.service.CartService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.UUID;
 
 @Controller
 public class CartController {
+
+
+    private final CartService cartService;
+
+    public CartController(CartService cartService) {
+        this.cartService = cartService;
+    }
 
     @GetMapping("/cart")
     public String seeCart(Model model){
@@ -17,4 +29,15 @@ public class CartController {
 
         return "cart/show-cart";
     }
+
+    @GetMapping("/addToCart/{productId}/{quantity}")
+    public String addToCart(@PathVariable("productId") UUID productId,
+                            @PathVariable("quantity") Integer quantity){
+
+        cartService.addToCart(productId, quantity);
+
+        return "redirect:/cart";
+    }
+
+
 }
