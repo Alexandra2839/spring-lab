b69d338cf6ee946b0cea7bc424ebb67746219195
Sasha <alexandra.aslobodian@gmail.com>
2023-04-09 16:46:48 -0400

    created remaining entities

---
+++ lab-06-orm-ecommerce/src/main/java/com/learn/entity/CartItem.java
@@ -0,0 +1,22 @@
+package com.learn.entity;
+
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class CartItem extends BaseEntity{
+
+    private int quantity;
+
+    @ManyToOne
+    private Cart cart;
+
+    @ManyToOne
+    private Product product;
+}
