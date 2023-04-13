35acc6ebff980c790eaf9349501ec12bcf559410
Sasha <alexandra.aslobodian@gmail.com>
2023-04-13 17:29:00 -0400

    practiced flyway

--- lab-05-data-cinema/src/main/java/com/learn/CinemaApplication.java
+++ lab-05-data-cinema/src/main/java/com/learn/CinemaApplication.java
@@ -1,7 +1,12 @@
 package com.learn;
 
+//import org.flywaydb.core.Flyway;
+//import org.flywaydb.core.api.output.MigrateResult;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import javax.sql.DataSource;
 
 @SpringBootApplication
 public class CinemaApplication {
@@ -9,5 +14,12 @@ public class CinemaApplication {
     public static void main(String[] args) {
         SpringApplication.run(CinemaApplication.class, args);
     }
+
+//    @Bean
+//    public MigrateResult migrateResult(DataSource dataSource){ //whenever I put dependency flyway is looking for data source
+//        return Flyway.configure().baselineOnMigrate(true).dataSource(dataSource).load().migrate();
+//    }
+
+
 
 }
