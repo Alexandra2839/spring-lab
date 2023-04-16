0f268ae38ab07eb0315d23ab1c0be7caf351fea1
Sasha <alexandra.aslobodian@gmail.com>
2023-04-16 15:20:10 -0400

    genre repository completed

--- lab-05-data-cinema/src/main/java/com/learn/repository/GenreRepository.java
+++ lab-05-data-cinema/src/main/java/com/learn/repository/GenreRepository.java
@@ -2,7 +2,10 @@ package com.learn.repository;
 
 import com.learn.entity.Genre;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface GenreRepository extends JpaRepository<Genre, Long> {
@@ -10,11 +13,15 @@ public interface GenreRepository extends JpaRepository<Genre, Long> {
     // ------------------- JPQL QUERIES ------------------- //
 
     //Write a JPQL query that return all genres
+    @Query("select g from Genre g")
+    List<Genre> findAll();
 
 
     // ------------------- Native QUERIES ------------------- //
 
     //Write a native query that returns genres by containing name
+    @Query(value = "select * from genre where name ILIKE concat('%',?1,'%') ", nativeQuery = true)
+    List<Genre> fetchAllContainingName(String name);
 
 
 }
