7ece495b3fc31f42768d45d15635c006914a0421
Sasha <alexandra.aslobodian@gmail.com>
2023-05-01 07:21:50 -0400

    testing table creation and data insertion with flyway

--- lab-05-data-cinema/src/main/java/com/learn/CinemaApplication.java
+++ lab-05-data-cinema/src/main/java/com/learn/CinemaApplication.java
@@ -1,7 +1,7 @@
 package com.learn;
 
-//import org.flywaydb.core.Flyway;
-//import org.flywaydb.core.api.output.MigrateResult;
+import org.flywaydb.core.Flyway;
+import org.flywaydb.core.api.output.MigrateResult;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
